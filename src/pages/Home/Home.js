6ec4ec13641ee5e7a3f80ec4475bae1601466a84
AristideExe <aristidepr@gmail.com>
2024-01-31 19:01:47 +0100

    Remplie la page d'accueil

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,11 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
       <div className={styles.home}>
-        <p>Home</p>
+        <p className={styles.welcome}>Bienvenue</p>
+        <h1>Je suis Aristide PRORIOL</h1>
+        <p className={styles.subtitle}>
+          Étudiant en 3e année de BUT Informatique, et développeur en alternance chez Atol Conseils et Développements
+        </p>
       </div>
     );
   };
