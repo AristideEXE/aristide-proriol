442ffc56ba5d66cb193c79ddbf19ee99c8f6bd58
AristideExe <aristidepr@gmail.com>
2024-02-01 00:04:10 +0100

    Ajoute l'icone hamburger pour accéder au sommaire

--- src/pages/Layout/Header/Header.js
+++ src/pages/Layout/Header/Header.js
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
-import { homePath } from "../../../routes";
+import { homePath, summaryPath } from "../../../routes";
 
 const Header = () => {
+  const currentPath = useLocation().pathname;
+
   return (
     <div className={styles.header}>
-      <Link to={homePath} className={styles.homeLink}>
+      <Link to={homePath} className={styles.link}>
         Aristide Proriol
       </Link>
+      <div>
+        {currentPath === summaryPath ? (
+          <Link to={-1} className={styles.link}>
+            <span class="material-symbols-outlined">close</span>
+          </Link>
+        ) : (
+          <Link to={summaryPath} className={styles.link}>
+            <span class="material-symbols-outlined">menu</span>
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
