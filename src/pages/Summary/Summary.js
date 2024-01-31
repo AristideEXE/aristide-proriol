320fd9512b14cdf7001659d1e9d76287c9394739
AristideExe <aristidepr@gmail.com>
2024-01-31 23:47:07 +0100

    Améliore l'affichage de la page sommaire

--- src/pages/Summary/Summary.js
+++ src/pages/Summary/Summary.js
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
 import Title from "../../components/Title/Title";
 import styles from "./Summary.module.css";
+import { contactPath, homePath } from "../../routes";
 
 const Summary = () => (
     <div className={styles.summary}>
         <Title title="Sommaire" />
-        <p>Toto</p>
+        <div className={styles.list}>
+            <Link className={styles.link} to={homePath}>
+                <p><span className={styles.number}>01</span>Accueil</p>
+            </Link>
+            <Link className={styles.link} to={homePath}>
+                <p><span className={styles.number}>02</span>Présentation</p>
+            </Link>
+            <Link className={styles.link} to={homePath}>
+                <p><span className={styles.number}>03</span>Projets</p>
+            </Link>
+            <Link className={styles.link} to={contactPath}>
+                <p><span className={styles.number}>04</span>Contact</p>
+            </Link>
+        </div>
     </div>
 );
 
