7ebafcee89038689a77cfab186aa72b2698da270
AristideEXE <aristidepr@gmail.com>
2024-02-22 18:47:11 +0100

    Ajoute le contenu de la page IRD Dispositifs

--- src/pages/Projects/Projects/IRDDispositifs/IRDDispositifs.js
+++ src/pages/Projects/Projects/IRDDispositifs/IRDDispositifs.js
@@ -1,11 +1,37 @@
 import BackToProjects from "../../../../components/BackToProjects/BackToProjects";
+import FadeIn from "../../../../components/FadeIn/FadeIn";
+import Title from "../../../../components/Title/Title";
 import styles from "./IRDDispositifs.module.css";
 
 const IRDDispositifs = () => (
     <>
-        <BackToProjects />
-        <div>
-            <p>IRD Dispositifs</p>
+        {/* <BackToProjects /> */}
+        <div className={styles.project}>
+            <Title title="IRD Dispositifs" />
+            
+            <div className={styles.content}>
+                <FadeIn delay="0.5s" duration="0.5s">
+                    <p>
+                        IRD Dispositifs est un progiciel de suivi de projets fait dans le cadre de mon alternance chez Atol Conseils et Développements.
+                        Le logiciel a été développé pour l'IRD, l'Institut de Recherche pour le Développement, dans le cadre de l'informatisation de leur système d'information.
+                    </p>
+                </FadeIn>
+                
+                <FadeIn delay="0.8s" duration="0.5s">
+                    <div className={styles.specifications}>
+                        <div>
+                            <p>ANNÉE</p>
+                            <p>TECHNOLOGIES</p>
+                            <p>CADRE</p>
+                        </div>
+                        <div>
+                            <p>2023 - En cours</p>
+                            <p>React JS - Java 17 - PostgreSQL - GraphQL</p>
+                            <p>Alternance chez Atol Conseils et Développements</p>
+                        </div>
+                    </div>
+                </FadeIn>
+            </div>
         </div>
     </>
 );
